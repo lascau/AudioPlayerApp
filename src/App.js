701ee8fc62fc2216aa525dc4c94c9d0ab7abb3d1
Lăscău Ionut Sebastian <lascauionutsebastian98@gmail.com>
2022-07-11 20:50:35 +0300

    building the media player ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MusicPlayer } from "./components/MusicPlayer";
+import Box from "@mui/material/Box";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <Box
+                m={3}
+                display="flex"
+                alignItems="center"
+                flexDirection="column"
+            >
+                <MusicPlayer musicUrl="https://v16-webapp.tiktok.com/931d806c2a22635cf706ff173833b9b9/62cc9c25/video/tos/useast2a/tos-useast2a-ve-0068c003/dbbe60211aae428fa204c785e1b72814/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=850&bt=425&btag=80000&cs=0&ds=3&ft=eXd.6HnMMyq8Z6ddXwe2NXq6yl7Gb&mime_type=video_mp4&qs=0&rc=Ojw1OGZmZTRpM2c0ODZkNEBpMzxuO2Y6Zm9sPDMzNzczM0AtMTQuYTQuNl8xMS5gMzBhYSNjNnBecjQwL25gLS1kMTZzcw%3D%3D&l=2022071115535201018905301602266B3A" />
+            </Box>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/MusicPlayer.js
@@ -0,0 +1,105 @@
+import React, { useState, useRef } from "react";
+import Stack from "@mui/material/Stack";
+import Slider from "@mui/material/Slider";
+import VolumeDown from "@mui/icons-material/VolumeDown";
+import VolumeUp from "@mui/icons-material/VolumeUp";
+import PauseIcon from "@mui/icons-material/Pause";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import ReplayIcon from "@mui/icons-material/Replay";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+
+const cardStyle = {
+    width: "500px",
+    height: "800px",
+};
+
+export const MusicPlayer = (props) => {
+    const [isAudioPlaying, setIsAudioPlaying] = useState(true);
+    const [currentTime, setCurrentTime] = useState("00:00");
+    const [_volume, setVolume] = useState(1);
+    const audioRef = useRef(new Audio(props.musicUrl));
+    const [audioDuration, setAudioDuration] = useState(null);
+
+    const display = (time) => {
+        if (time) {
+            const seconds = Math.floor(time % 60);
+            const minutes = Math.floor(time / 60);
+            const stringMinutes =
+                minutes >= 0 && minutes <= 9 ? `0${minutes}` : `${minutes}`;
+            const stringSeconds =
+                seconds >= 0 && seconds <= 9 ? `0${seconds}` : `${seconds}`;
+            return stringMinutes + ":" + stringSeconds;
+        }
+        return "00:00";
+    };
+
+    const updateCurrentTime = (time) => {
+        setCurrentTime(display(time));
+    };
+
+    audioRef.current.ontimeupdate = () => {
+        setCurrentTime(audioRef.current.currentTime.toFixed(0));
+        updateCurrentTime(audioRef.current.currentTime);
+    };
+
+    audioRef.current.onloadedmetadata = () => {
+        setAudioDuration(audioRef.current.duration.toFixed(0));
+    };
+
+    const startAudio = () => {
+        audioRef.current.play();
+        setIsAudioPlaying(!isAudioPlaying);
+    };
+
+    const pauseAudio = () => {
+        audioRef.current.pause();
+        setIsAudioPlaying(!isAudioPlaying);
+    };
+
+    const replayAudio = () => {
+        audioRef.current.currentTime = 0;
+        audioRef.current.play();
+    };
+
+    const changeVolume = (event, value) => {
+        setVolume(value);
+        audioRef.current.volume = value / 100;
+    };
+
+    return (
+        <>
+            <Card style={cardStyle}>
+                <audio ref={audioRef} src={props.musicUrl} preload="metadata" />
+                {audioDuration && (
+                    <div>
+                        {currentTime}/{display(audioDuration)}
+                    </div>
+                )}
+                {isAudioPlaying ? (
+                    <PlayArrowIcon onClick={startAudio} />
+                ) : (
+                    <PauseIcon onClick={pauseAudio} />
+                )}
+                <ReplayIcon onClick={replayAudio} />
+                <Stack spacing={2} sx={{ height: 300 }} alignItems="center">
+                    <Slider
+                        aria-label="Volume"
+                        value={_volume}
+                        orientation="vertical"
+                        defaultValue={30}
+                        onChange={changeVolume}
+                    />
+                    {_volume === 0 ? (
+                        <VolumeOffIcon />
+                    ) : _volume >= 50 ? (
+                        <VolumeUp />
+                    ) : (
+                        <VolumeDown />
+                    )}
+                </Stack>
+            </Card>
+        </>
+    );
+};

af7070cdb11ae610f64c5a29f48ecbf0facd25d9
Lăscău Ionut Sebastian <lascauionutsebastian98@gmail.com>
2022-07-11 23:03:39 +0300

    added fastForward and timer progress bar

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -8,6 +8,9 @@ import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import ReplayIcon from "@mui/icons-material/Replay";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import Card from "@mui/material/Card";
+import LinearProgress from "@mui/material/LinearProgress";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import Box from "@mui/material/Box";
 
 const cardStyle = {
@@ -18,9 +21,10 @@ const cardStyle = {
 export const MusicPlayer = (props) => {
     const [isAudioPlaying, setIsAudioPlaying] = useState(true);
     const [currentTime, setCurrentTime] = useState("00:00");
-    const [_volume, setVolume] = useState(1);
+    const [_volume, setVolume] = useState(50);
     const audioRef = useRef(new Audio(props.musicUrl));
     const [audioDuration, setAudioDuration] = useState(null);
+    const [audioProgress, setAudioProgress] = useState(0);
 
     const display = (time) => {
         if (time) {
@@ -42,6 +46,7 @@ export const MusicPlayer = (props) => {
     audioRef.current.ontimeupdate = () => {
         setCurrentTime(audioRef.current.currentTime.toFixed(0));
         updateCurrentTime(audioRef.current.currentTime);
+        setAudioProgress((audioRef.current.currentTime * 100) / audioDuration);
     };
 
     audioRef.current.onloadedmetadata = () => {
@@ -68,27 +73,48 @@ export const MusicPlayer = (props) => {
         audioRef.current.volume = value / 100;
     };
 
+    const rightFastForward = () => {
+        audioRef.current.currentTime += 5;
+        setCurrentTime(audioRef.current.currentTime.toFixed(0));
+        updateCurrentTime(audioRef.current.currentTime);
+        setAudioProgress((audioRef.current.currentTime * 100) / audioDuration);
+    };
+
+    const leftFastForward = () => {
+        audioRef.current.currentTime -= 5;
+        setCurrentTime(audioRef.current.currentTime.toFixed(0));
+        updateCurrentTime(audioRef.current.currentTime);
+        setAudioProgress((audioRef.current.currentTime * 100) / audioDuration);
+    };
+
     return (
         <>
             <Card style={cardStyle}>
                 <audio ref={audioRef} src={props.musicUrl} preload="metadata" />
                 {audioDuration && (
-                    <div>
-                        {currentTime}/{display(audioDuration)}
-                    </div>
+                    <>
+                        <div>
+                            {currentTime}/{display(audioDuration)}
+                        </div>
+                        <LinearProgress
+                            variant="determinate"
+                            value={audioProgress}
+                        />
+                    </>
                 )}
+                <KeyboardDoubleArrowLeftIcon onClick={leftFastForward} />
                 {isAudioPlaying ? (
                     <PlayArrowIcon onClick={startAudio} />
                 ) : (
                     <PauseIcon onClick={pauseAudio} />
                 )}
+                <KeyboardDoubleArrowRightIcon onClick={rightFastForward} />
                 <ReplayIcon onClick={replayAudio} />
                 <Stack spacing={2} sx={{ height: 300 }} alignItems="center">
                     <Slider
                         aria-label="Volume"
                         value={_volume}
                         orientation="vertical"
-                        defaultValue={30}
                         onChange={changeVolume}
                     />
                     {_volume === 0 ? (

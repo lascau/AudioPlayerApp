9ae43eb7e2845cb294c8fc86e5b8947163a89aea
Lăscău Ionut Sebastian <lascauionutsebastian98@gmail.com>
2022-07-13 21:51:53 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Box from "@mui/material/Box";
 import { PlayList } from "./components/PlayList";
 
 const App = () => {
-    const _songsUrl = [
-        "https://v16-webapp.tiktok.com/1340aaa8558155c479658b6fcd485676/62cf0575/video/tos/useast2a/tos-useast2a-pve-0068/3ca1e1e14a12441e840bfd69b0a19cd8/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=3270&bt=1635&btag=80000&cs=0&ds=3&ft=eXd.6HnMMyq8ZrsfXwe2NNcoyl7Gb&mime_type=video_mp4&qs=0&rc=Ojo5Mzw1OTQ5NTg4ZDNmM0BpajlrdWQ6ZjlkZTMzNzczM0AvMDMxLWI2XzUxMDQuXl4vYSNsbGpjcjRfZjBgLS1kMTZzcw%3D%3D&l=20220713114826010223086021010F8F2B",
-        "https://v16-webapp.tiktok.com/8798a61eefdd39da8864e2e54554bead/62cf05ba/video/tos/useast2a/tos-useast2a-ve-0068c001/0a91e10031d54732b294e25ecb1aa170/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=4156&bt=2078&btag=80000&cs=0&ds=3&ft=eXd.6HnMMyq8ZrsfXwe2NNcoyl7Gb&mime_type=video_mp4&qs=0&rc=NDk6ZzU8N2Y5OTk2PDg2ZUBpM29keTw6ZmxkZTMzNzczM0AvLS8vLl5iNjAxXmJjNmJhYSMyaWJycjRfcDNgLS1kMTZzcw%3D%3D&l=20220713114826010223086021010F8F2B",
-    ];
+    const _songsUrl = [];
+
+    useEffect(() => {
+        fetch("https://mocki.io/v1/e3653440-6df3-498f-8dc0-8f213cff0ae5")
+            .then((val) => val.json())
+            .then((data) => {
+                for (let index = 0; index < data.length; index++) {
+                    _songsUrl.push(data[0]["songAddres"]);
+                }
+            });
+    }, []);
 
     return (
         <div>
